14118f56bb42d9de5d2224a3902f1cb5cdb24215
Simon Moos <simon.moos95@gmail.com>
2017-08-30 23:17:03 +0200

    Make Matrix a generic class

--- main.cpp
+++ main.cpp
@@ -2,37 +2,35 @@
 
 #include "matrix.h"
 
-float calculate_weight(const Matrix &pattern, size_t i, size_t j)
+float calculate_weight(const Pattern &pattern, size_t i, size_t j)
 {
     if (i == j) return 0.0f;
-    return (1.0f / pattern.num_neurons) * pattern.get_linear(i) * pattern.get_linear(j);
+    return (1.0f / pattern.num_elements) * pattern.get_linear(i) * pattern.get_linear(j);
 }
 
-void create_weight_matrix(std::vector<float> &weights, const Matrix &pattern)
+WeightMatrix create_weight_matrix(const Pattern &pattern)
 {
-    size_t weight_matrix_side = pattern.num_neurons;
-    size_t weight_matrix_size = weight_matrix_side * weight_matrix_side;
-
-    // Resize to fit a NxN weight matrix
-    weights.resize(weight_matrix_size);
+    size_t weight_matrix_side = pattern.num_elements;
+    WeightMatrix weights(weight_matrix_side, weight_matrix_side, {});
 
     for (size_t i = 0; i < weight_matrix_side; ++i)  {
         for (size_t j = 0; j < weight_matrix_side; ++j)  {
-            weights[j + i * weight_matrix_side] = calculate_weight(pattern, i, j);
+            weights.set(i, j, calculate_weight(pattern, i, j));
         }
     }
+
+    return weights;
 }
 
 int sgn(float x) {
     return (0 < x) - (x < 0);
 }
 
-void update_neuron(Matrix& pattern, const std::vector<float>& weights, size_t i)
+void update_neuron(Pattern& pattern, const WeightMatrix& weights, size_t i)
 {
     float h = 0.0f;
-    for (size_t j = 0; j < pattern.num_neurons; ++j) {
-        float Wij = weights[j + i * pattern.num_neurons]; // TODO: make matrix class or similar for weights to keep track of size etc.
-        h += Wij * pattern.get_linear(j);
+    for (size_t j = 0; j < pattern.num_elements; ++j) {
+        h += weights.get(i, j) * pattern.get_linear(j);
     }
     int new_value = sgn(h);
 
@@ -43,9 +41,7 @@ int main()
 {
     std::srand(static_cast<uint>(std::time(0)));
 
-    std::vector<float> weights;
-
-    Matrix pattern = Matrix(6, 9, {
+    Pattern pattern(6, 9, {
             -1, -1, -1, -1, -1, -1,
             -1, +1, +1, +1, +1, -1,
             -1, +1, -1, -1, -1, -1,
@@ -56,7 +52,7 @@ int main()
             -1, +1, +1, +1, +1, -1,
             -1, -1, -1, -1, -1, -1
     });
-    Matrix current_state = Matrix(6, 9, {
+    Pattern current_state(6, 9, {
             +1, -1, +1, -1, -1, -1,
             -1, +1, -1, +1, +1, -1,
             -1, +1, -1, -1, +1, -1,
@@ -69,7 +65,7 @@ int main()
     });
 
     // Assign weights according to the ... formula
-    create_weight_matrix(weights, pattern);
+    WeightMatrix weights = create_weight_matrix(pattern);
 
     std::cout << "Goal pattern:" << std::endl;
     pattern.debug_print();
@@ -77,16 +73,19 @@ int main()
     std::cout << "Initial state:" << std::endl;
     current_state.debug_print();
 
+/*
+    // NOTE: CLion *incorrectly* states that the == is always true... Not sure how to fix that.
     if (pattern == current_state) {
         std::cout << "The initial state is already equal to the pattern." << std::endl;
         return 0;
     }
+*/
 
     constexpr int MAX_NUM_ITERATIONS = 10000;
     for (int iteration = 1; iteration <= MAX_NUM_ITERATIONS; iteration++) {
 
         // Pick random neuron to update
-        size_t i = std::rand() % pattern.num_neurons;
+        size_t i = std::rand() % pattern.num_elements;
         update_neuron(current_state, weights, i);
 
         // Display pattern
--- matrix.h
+++ matrix.h
@@ -6,34 +6,124 @@
 #define CONVERGINGPATTERNS_PATTERN_H
 
 #include <vector>
+#include <cassert>
 #include <initializer_list>
 
+template<typename T>
 class Matrix
 {
 public:
 
-    Matrix(const size_t width, const size_t height, const std::initializer_list<int>& pattern);
+    Matrix(const size_t width, const size_t height, const std::initializer_list<T>& pattern);
     ~Matrix() {}
 
     const size_t width;
     const size_t height;
-    const size_t num_neurons;
+    const size_t num_elements;
 
-    int get_linear(size_t i) const;
-    void set_linear(size_t i, int value);
+    T get_linear(size_t i) const;
+    void set_linear(size_t i, T value);
 
-    int get(size_t i, size_t j) const;
-    void set(size_t i, size_t j, int value);
+    T get(size_t i, size_t j) const;
+    void set(size_t i, size_t j, T value);
 
     void debug_print(bool transform_values = true) const;
 
-    bool operator==(const Matrix& other) const;
-
 private:
 
-    std::vector<int> state;
+    std::vector<T> state;
 
 };
 
+////////////////////////////
+
+typedef Matrix<int> Pattern;
+typedef Matrix<float> WeightMatrix;
+
+////////////////////////////
+////// Implementation //////
+////////////////////////////
+
+template<typename T>
+Matrix<T>::Matrix(const size_t width, const size_t height, const std::initializer_list<T>& pattern)
+        : width(width)
+        , height(height)
+        , num_elements(width * height)
+{
+    unsigned long size = static_cast<unsigned long>(width * height);
+    if (pattern.size()) {
+        state.reserve(size);
+        state.assign(pattern.begin(), pattern.end());
+    } else {
+        state.resize(size);
+    }
+}
+
+template<typename T>
+void
+Matrix<T>::debug_print(bool transform_values) const
+{
+    for (size_t i = 0; i < height; ++i) {
+        for (size_t j = 0; j < width; ++j) {
+            int val = get(i, j);
+            val = (transform_values) ? (val + 1) / 2 : val;
+            std::cout << " " << val;
+        }
+        std::cout << std::endl;
+    }
+}
+
+template<typename T>
+T
+Matrix<T>::get_linear(size_t i) const
+{
+    assert(i >= 0 && i < num_elements);
+    return state[i];
+}
+
+template<typename T>
+void
+Matrix<T>::set_linear(size_t i, T value)
+{
+    assert(i >= 0 && i < num_elements);
+    state[i] = value;
+}
+
+template<typename T>
+T
+Matrix<T>::get(size_t i, size_t j) const
+{
+    assert(i >= 0 && i < height);
+    assert(j >= 0 && j < width);
+    return state[j + i * width];
+}
+
+template<typename T>
+void
+Matrix<T>::set(size_t i, size_t j, T value)
+{
+    assert(i >= 0 && i < height);
+    assert(j >= 0 && j < width);
+    state[j + i * width] = value;
+}
+
+////////////////////////////
+
+template<typename T>
+bool
+operator==(const Matrix<T>& first, const Matrix<T>& second)
+{
+    if (first.num_elements != second.num_elements) return false;
+    if (first.width  != second.width) return false;
+    if (first.height != second.height) return false;
+
+    for (size_t i = 0; i < first.num_elements; ++i) {
+        if (first.get_linear(i) != second.get_linear(i)) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 #endif //CONVERGINGPATTERNS_PATTERN_H
