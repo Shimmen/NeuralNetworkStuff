d17d19ff38bbc859d37bf44b2f727c604d15cad8
Simon Moos <simon.moos95@gmail.com>
2017-08-30 23:17:46 +0200

    Reformat code

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 
 #include "matrix.h"
 
-float calculate_weight(const Pattern &pattern, size_t i, size_t j)
+float
+calculate_weight(const Pattern& pattern, size_t i, size_t j)
 {
     if (i == j) return 0.0f;
     return (1.0f / pattern.num_elements) * pattern.get_linear(i) * pattern.get_linear(j);
 }
 
-WeightMatrix create_weight_matrix(const Pattern &pattern)
+WeightMatrix
+create_weight_matrix(const Pattern& pattern)
 {
     size_t weight_matrix_side = pattern.num_elements;
     WeightMatrix weights(weight_matrix_side, weight_matrix_side, {});
@@ -22,11 +24,13 @@ WeightMatrix create_weight_matrix(const Pattern &pattern)
     return weights;
 }
 
-int sgn(float x) {
+int
+sgn(float x) {
     return (0 < x) - (x < 0);
 }
 
-void update_neuron(Pattern& pattern, const WeightMatrix& weights, size_t i)
+void
+update_neuron(Pattern& pattern, const WeightMatrix& weights, size_t i)
 {
     float h = 0.0f;
     for (size_t j = 0; j < pattern.num_elements; ++j) {
@@ -37,7 +41,8 @@ void update_neuron(Pattern& pattern, const WeightMatrix& weights, size_t i)
     pattern.set_linear(i, new_value);
 }
 
-int main()
+int
+main()
 {
     std::srand(static_cast<uint>(std::time(0)));
 
